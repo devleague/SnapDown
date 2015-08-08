457f444a449bf461b149d720b5e5fa4de3566293
danturne <daniel.turner@gmail.com>
2015-08-07 20:49:37 -1000

    routes almost complete, not debugged

---
+++ server/routes/challenge_user_routes.js
@@ -0,0 +1,129 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+// var ChallengeUser = require('../models/ChallengeUser').ChallengeUser;
+
+router.get('/', function(req,res) {
+
+  db.findAll()
+
+    .then(function(error, results) {
+
+      res.json(results);
+    });
+});
+
+router.get('/:id', function(req,res) {
+
+  db.findOne({
+
+    where: {
+
+      challenge_user_id: req.params.id
+    }
+
+  }).then(function(result) {
+
+    if(result) {
+
+      res.json(result);
+
+    } else {
+
+      res.status(404);
+      res.send("Could not locate the requested resource.");
+    }
+  })
+});
+
+router.post('/', function(req,res) {
+
+  db.create({
+
+    // user_id: req.body.user_id,
+    // start_at: req.body.start_at,
+    // expire_at: req.body.expire_at,
+    // name: req.body.name
+
+  }).then(function(result) { //may be unnecessary
+
+    res.status(200).json(result);
+});
+
+router.put('/:id', function(req,res) {
+
+  db.findOne({
+
+    where: {
+
+      challenge_user_id: req.params.id
+    }
+
+  }).then(function(result){
+
+    if(!result) {
+
+      res.status(404);
+      res.send("Could not locate the requested resource.");
+    }
+
+    // if(req.body.user_id !== undefined) {
+
+    //   result.dataValues.user_id = req.body.user_id;
+    // }
+
+    // if(req.body.start_at !== undefined) {
+
+    //   result.dataValues.start_at = req.body.start_at;
+    // }
+
+    // if(req.body.expire_at !== undefined) {
+
+    //   result.dataValues.expire_at = req.body.expire_at;
+    // }
+
+    // if(req.body.name !== undefined) {
+
+    //   result.dataValues.name = req.body.name;
+    // }
+
+    result.save({
+
+      // fields: ['user_id','start_at','expire_at','name']
+
+    }).then(function(result) {
+
+      res.status(200);
+      res.send(result);
+    });
+  });
+});
+
+router.delete('/:id', function(req,res) {
+
+  var id = req.params.id;
+
+  db.findOne({
+
+    where: {
+
+      challenge_user_id: id
+    }
+
+  }).then(function(result) {
+
+    if(!result) {
+
+      res.status(404);
+      res.send("Could not locate the requested resource.");
+
+    } else {
+
+      result.destroy().then(function() {
+
+        res.status(200).send();
+      });
+    }
+  });
+});
