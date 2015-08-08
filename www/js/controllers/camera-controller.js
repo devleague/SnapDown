6a17abe0aa6a30bd4a4e7572a587a378f1b83732
Constantino <Coproductions@Constantins-MacBook-Pro.local>
2015-08-07 17:11:27 -1000

    setting up camera plugin

--- www/js/controllers/camera-controller.js
+++ www/js/controllers/camera-controller.js
@@ -5,12 +5,25 @@ angular.module('starter')
 
 
   $scope.pictureURL = 'http://placehold.it/300x300';
+  $scope.takePicture = function(){
+
+  };
 
   $scope.getPhoto = function() {
-    Camera.getPicture().then(function(imageURI) {
+    Camera.getPicture({
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false
+    }).then(function(imageURI) {
       console.log(imageURI);
+      if(imageURI){
+
+        $scope.lastPhoto = imageURI;
+      }
     }, function(err) {
       console.err(err);
     });
   };
+
 });
