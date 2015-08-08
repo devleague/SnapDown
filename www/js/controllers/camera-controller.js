6c6c5205e85ddf411eaa2df3329ae487caa8e8e9
Constantino <Coproductions@Constantins-MacBook-Pro.local>
2015-08-07 16:35:12 -1000

    starting camera feature

--- www/js/controllers/camera-controller.js
+++ www/js/controllers/camera-controller.js
@@ -2,11 +2,15 @@ angular.module('starter')
 
 .controller('camera-controller', function($scope, Camera) {
 
-    $scope.getPhoto = function() {
-      Camera.getPicture().then(function(imageURI) {
-        console.log(imageURI);
-      }, function(err) {
-        console.err(err);
-      });
-    };
+
+
+  $scope.pictureURL = 'http://placehold.it/300x300';
+
+  $scope.getPhoto = function() {
+    Camera.getPicture().then(function(imageURI) {
+      console.log(imageURI);
+    }, function(err) {
+      console.err(err);
+    });
+  };
 });
