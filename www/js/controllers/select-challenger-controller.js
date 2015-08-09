cccba9c24dedd9d3875f63ccae7044d0dc361f85
jasontsukamoto <jasontsukamoto@gmail.com>
2015-08-08 19:10:52 -1000

    fix merge conflicts

--- www/js/controllers/select-challenger-controller.js
+++ www/js/controllers/select-challenger-controller.js
@@ -1,10 +1,7 @@
 angular.module('starter')
 
-<<<<<<< HEAD
+
 .controller('select-challenger-controller', function($scope,$stateParams) {
   $scope.imageURI = $stateParams.imageURI;
-=======
-.controller('select-challenger-controller', function($scope) {
 
->>>>>>> 6192316a8507ed541a868944c1bf21856310bed3
 });
