91dfcb43fc132c2c64d95a1f54b5b66debb7c3bd
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-12 20:19:39 +0000

    added user if not in system

--- server/routes/fbauth_routes.js
+++ server/routes/fbauth_routes.js
@@ -10,15 +10,31 @@ var users = require('../models').User;
 router.post('/', function(req, res) {
 	console.log(req.body);
 
-	users.create({
-		first_name: req.body.first_name,
-		last_name: req.body.last_name,
-		facebook_id:req.body.id,
-		facebook_image_url:req.body.picture,
-		email:req.body.email,
-	});
+	users.findOne({where:{
+		facebook_id:req.body.id
+	}}).then(function(result){
+		if(!result){
+			users.create({
+				first_name: req.body.first_name,
+				last_name: req.body.last_name,
+				facebook_id: req.body.id,
+				facebook_image_url: req.body.picture,
+				email: req.body.email,
+			});
+			console.log('---------------------------------');
+			console.log('user added')
+			console.log('---------------------------------');
 
-	res.send('user created');
+			res.send('user added')
+		}else{
+			console.log('---------------------------------');
+			console.log('user denied.  already in system')
+			console.log('---------------------------------');
+
+			res.send('user already in system');
+		}
+	})
+
 });
 
 
