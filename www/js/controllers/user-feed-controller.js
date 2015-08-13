ca1951a16e2dc012d5e30ae83c56876bd312ede4
jasontsukamoto <jasontsukamoto@gmail.com>
2015-08-12 14:38:14 -1000

    adds swipe to user feed

--- www/js/controllers/user-feed-controller.js
+++ www/js/controllers/user-feed-controller.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-.controller('user-feed-controller', function($scope, ChallengeService, $state) {
+.controller('user-feed-controller', function($scope, ChallengeService, $state, $ionicModal) {
 
   $scope.getMyChallenges = function (){
     ChallengeService.getMyChallenges()
@@ -11,6 +11,21 @@ angular.module('starter')
         console.log('err w/ showing challeges', err);
       })
   }
+
+  $ionicModal.fromTemplateUrl('edit-profile-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
 
   $scope.onSwipeLeft = function() {
     $state.go('app.landing');
