7b12d76619a636e4523a466538aa62db0c0d197d
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-13 22:02:58 +0000

    filtered users.  working on removing timer upon completion

--- www/js/controllers/user-challenged-controller.js
+++ www/js/controllers/user-challenged-controller.js
@@ -1,5 +1,33 @@
 angular.module('starter')
 
-.controller('detail-view-controller', function($scope) {
+.controller('user-challenged-controller', function($location,$scope, $ionicPlatform, ChallengeService, DataSharingService,$rootScope) {
+	$scope.allChallengers = [];
 
+	$scope.getChallengeContext = function() {
+		var challengeId = DataSharingService.activeChallenge.id;
+		ChallengeService.getChallengeContext(challengeId)
+			.success(function(res) {
+				console.log('challenge context', res);
+				$scope.allChallengers = res.challenge.Challengers;
+				console.log('all challengers', $scope.allChallengers)
+			})
+			.error(function(err) {
+				console.log('err w/challenge context', err);
+			})
+	}
+
+	$scope.expireTime = function() {
+		if (Date.now() < DataSharingService.activeChallenge.expireAt) {
+			$scope.timerComplete = 'timer stopped';
+			var currentPath = $location.path();
+			$location.path(currentPath);
+			var timer = document.querySelector('.timer');
+			console.log('timer',timer);
+			$rootScope.apply();
+		}
+	};
+
+	$ionicPlatform.ready(function() {
+		$scope.getChallengeContext();
+	});
 });
