ee3f9a045d8742fe042b4179247aee4cb18e268a
danturne <daniel.turner@gmail.com>
2015-08-14 09:51:53 -1000

    switching branches

--- server/routes/image_s3_routes.js
+++ server/routes/image_s3_routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// var AWS = require('aws-sdk');
-// var uuid = require('node-uuid');
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
 var express = require('express');
 var router = express.Router();
 var db = require('../models').Image;
@@ -11,7 +11,7 @@ var S3_FOLDER = "images/";
 
 router.post('/', function(req,res) {
 
-  console.log(req);
+  // console.log(req);
 
   var keyName = S3_FOLDER + uuid.v4() + ".jpg";
 
@@ -21,6 +21,7 @@ router.post('/', function(req,res) {
   s3.upload({Body: req.body})
     .on('httpUploadProgress', function(evt) { console.log(evt); }).
     send(function(err, data) { console.log(err, data) });
+
   //TODO create image record on database
   res.status(200).send("done");
 });
