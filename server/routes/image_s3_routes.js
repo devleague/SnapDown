9d4c119f7a1bc3b2412c8ffe737545f793f96f33
danturne <daniel.turner@gmail.com>
2015-08-14 11:17:39 -1000

    upload and database image item creation complete, ready for test

--- server/routes/image_s3_routes.js
+++ server/routes/image_s3_routes.js
@@ -11,19 +11,53 @@ var S3_FOLDER = "images/";
 
 router.post('/', function(req,res) {
 
-  // console.log(req);
+  if(!req.body.base64Image) {
 
+    res.status(400).send("No image attached to image upload request");
+  }
+
+  if(!req.body.challenger_id) {
+
+    res.status(400).send("No id attached to image upload request");
+  }
+
+  var s3 = new AWS.S3( { params: {Bucket: S3_BUCKET_NAME} } );
   var keyName = S3_FOLDER + uuid.v4() + ".jpg";
 
-  // var body = fs.createReadStream(req.body);
-  var s3 = new AWS.S3({params: {Bucket: S3_BUCKET_NAME, Key: keyName}});
+  var buffer = new Buffer(req.body.base64Image.replace(/^data:image\/\w+;base64,/, ""),'base64');
 
-  s3.upload({Body: req.body})
-    .on('httpUploadProgress', function(evt) { console.log(evt); }).
-    send(function(err, data) { console.log(err, data) });
+  console.log(buffer);
 
-  //TODO create image record on database
-  res.status(200).send("done");
+  var data = {
+
+    Key: keyName,
+    Body: buffer,
+    ContentEncoding: 'base64',
+    ContentType: 'image/jpeg'
+  };
+
+  s3.upload(data)
+    .on('httpUploadProgress', function(event) { console.log(event); })
+    .send(function(error, data) {
+
+      if(error) {
+
+        console.log(error);
+
+        res.status(500).send('Could not upload image');
+      }
+
+      db.create({
+
+        s3_reference: data.Location,
+        privacy_status: req.body.privacy_status,
+        challenger_id: req.body.challenger_id
+
+      }).then(function(result) { //may be unnecessary
+
+        res.status(200).send(data.Location);
+      });
+    });
 });
 
 module.exports = router;
