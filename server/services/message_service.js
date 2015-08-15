4b8d84eb92166b100ca0ed6d843368809fd3a42c
Judah Lamm <Judah.Lamm@gmail.com>
2015-08-15 11:47:11 -1000

    added new phone providers

--- server/services/message_service.js
+++ server/services/message_service.js
@@ -2,9 +2,13 @@
 
 module.exports = {
   from: 'SnapDown <me@snapdown.us>',
-  verizon: '@vtext.com',
   att: '@txt.att.net',
+  boostmobile : '@myboostmobile.com',
+  metropcs: '@mymetropcs.com',
+  sprint : '@messaging.sprintpcs.com',
   tmobile: '@tmomail.net',
+  verizon: '@vtext.com',
+  virginmobile : '@vmobl.com',
   subject: 'test subject',
   messageBody: 'invited you to a challenge ..... message Body'
 };
