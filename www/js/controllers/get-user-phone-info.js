0162247ac49e23ea83f5a0230271360d0df75b28
Judah Lamm <Judah.Lamm@gmail.com>
2015-08-15 10:03:41 -1000

    route is set up for updating the info

---
+++ www/js/controllers/get-user-phone-info.js
@@ -0,0 +1,18 @@
+angular.module('starter')
+
+.controller('get-user-phone-info', function ($localStorage){
+
+  var user_id = 2;
+  // var user_id =  $localStorage.activeUserId;
+
+  $scope.updateUserPhoneInfo = function (){
+    UserService.updateUserPhoneInfo(user_id, user_info)
+      .success(function (res){
+        console.log('updated user info', res);
+      })
+      .error(function (error){
+        console.log('error with updating a user phone info', error);
+      })
+  }
+
+})
