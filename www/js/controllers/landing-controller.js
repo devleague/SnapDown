d02702a6646f4829493d9c83dd6c038ce69992ee
jasontsukamoto <jasontsukamoto@gmail.com>
2015-08-14 19:16:53 -1000

    finishes styling on challenge complete page

--- www/js/controllers/landing-controller.js
+++ www/js/controllers/landing-controller.js
@@ -93,19 +93,7 @@ angular.module('starter')
       $state.go('app.user-feed');
     }
 
-    $ionicModal.fromTemplateUrl('edit-profile-modal.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
-    }).then(function(modal) {
-      $scope.modal = modal;
-    });
 
-    $scope.openModal = function() {
-      $scope.modal.show();
-    };
-    $scope.closeModal = function() {
-      $scope.modal.hide();
-    };
 
 
   })
--- www/js/controllers/user-feed-controller.js
+++ www/js/controllers/user-feed-controller.js
@@ -28,21 +28,6 @@ angular.module('starter')
   }
 
 
-  $ionicModal.fromTemplateUrl('edit-profile-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
-
-
   $scope.onSwipeLeft = function() {
     console.log('going left');
     $state.go('app.landing');
