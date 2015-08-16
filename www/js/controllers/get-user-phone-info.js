75ec3ab3bde72eda3e08caca041205019e75fffd
Judah Lamm <Judah.Lamm@gmail.com>
2015-08-15 15:31:44 -1000

    phone form is working without validations

--- www/js/controllers/get-user-phone-info.js
+++ www/js/controllers/get-user-phone-info.js
@@ -16,7 +16,6 @@ angular.module('starter')
       })
   }
 
-
   $scope.providers = [];
   ProviderService.getAllProviders()
   .success(function (res){
--- www/js/services.js
+++ www/js/services.js
@@ -215,15 +215,14 @@ function UserService($http) {
   }
 
   this.updateUserPhoneInfo = function (user_id, user_info){
-    console.log('user id', user_id);
-    console.log('user info', user_info.service_provider.id);
+    console.log('phoneincoming', user_info);
 
     var user_phone_info = {
       phone: user_info.phone,
       service_provider: user_info.service_provider.id
     }
-    console.log('updated info', user_phone_info);
     return $http.put('http://localhost:3000/api/users/' + user_id, user_phone_info)
+
   }
 
   // //not in any controller or funcitonality as now
