f8d47f814377fe067cc613407cbf9e5a6787daa9
danturne <daniel.turner@gmail.com>
2015-08-15 18:34:12 -1000

    provisional add to challenger routes

--- server/routes/challenger_routes.js
+++ server/routes/challenger_routes.js
@@ -63,6 +63,18 @@ router.get('/:id/challenges', function(req,res) {
   });
 });
 
+// router.get('/:id/images', function(req,res) {
+
+//   db.findAll({
+
+//     where { user_id: req.params.id },
+//     include :[
+//       { model: challenge},
+//       {model.images}]
+//     }).then(function(challengers)
+//   }
+// })
+
 //Gets the Challenger context
 router.get('/:id/context', function(req,res) {
 
