7cc41ec06615cc217f9458f13d1836235ab20bd7
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-16 19:06:03 +0000

    added forms for updating users

--- www/js/controllers/user-profile-controller.js
+++ www/js/controllers/user-profile-controller.js
@@ -1,7 +1,7 @@
 'use strict'
 angular.module('starter')
 
-.controller('user-profile-controller', function($scope, $localStorage, FacebookService, $state, $http, UserStatsService) {
+.controller('user-profile-controller', function($scope, ProviderService, $localStorage, FacebookService, $state, $http, UserStatsService) {
 
 	FacebookService.getUserFacebook().then(function(res) {
 		$scope.fb_user = res.data;
@@ -26,12 +26,22 @@ angular.module('starter')
 		$state.go('app.oauth')
 	}
 
-	$scope.update = function(email,password,phone,carrier){
-		console.log('email',email);
-		console.log('password',password);
-		console.log('phone',phone);
-		console.log('carrier',carrier);
+	$scope.update = function(email, password, phone, carrier) {
+		console.log('email', email);
+		console.log('password', password);
+		console.log('phone', phone);
+		console.log('carrier', carrier);
 	}
+
+	$scope.providers = [];
+	ProviderService.getAllProviders()
+		.success(function(res) {
+			console.log('providers', res)
+			$scope.providers = res;
+		})
+		.error(function(err) {
+			console.log('err', err);
+		})
 
 
 });
