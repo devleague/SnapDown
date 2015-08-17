54d0e289b504b2f2e7e53bd09d6935588410efb3
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-17 10:58:47 +0000

    rerouted to app/landing for development

--- www/js/app.js
+++ www/js/app.js
@@ -241,7 +241,7 @@ angular.module('starter', ['ionic',
 
   // if none of the above states are matched, use this as the fallback
 
-  $urlRouterProvider.otherwise('/app/oauth');
+  $urlRouterProvider.otherwise('/app/landing');
 
 });
 
