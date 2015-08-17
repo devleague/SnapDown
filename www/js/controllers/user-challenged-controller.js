4d1a3d6734d8a2d2069cccda2a225aadc25f3e59
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-17 11:59:39 +0000

    added missing parenthesis

--- www/js/controllers/user-challenged-controller.js
+++ www/js/controllers/user-challenged-controller.js
@@ -1,8 +1,8 @@
 angular.module('starter')
 
-.controller('user-challenged-controller', function($location, $localStorage, $scope, $ionicPlatform, ChallengeService, $stateParams,$rootScope, Camera, $timeout, UserStatsService) {
-	$scope.allChallengers = [];
-	var challengeId = $stateParams.activeChallengeId;
+.controller('user-challenged-controller', function($location, $localStorage, $scope, $ionicPlatform, ChallengeService, $stateParams, $rootScope, Camera, $timeout, UserStatsService) {
+  $scope.allChallengers = [];
+  var challengeId = $stateParams.activeChallengeId;
   var expireTime = $stateParams.activeChallengeExpireTime;
   $scope.expireTime = expireTime;
   var challengerId = $stateParams.challengerId;
@@ -10,74 +10,74 @@ angular.module('starter')
   $scope.getChallengeContext();
 
   $scope.getChallengeContext = function() {
-		ChallengeService.getChallengeContext(challengeId)
-			.success(function(res) {
-				$scope.challengeName = res.challenge.name;
-				console.log('challengeName', challengeName);
-				console.log('challenge context', res);
-				$scope.allChallengers = res.challenge.Challengers;
-				console.log('all challengers', $scope.allChallengers)
-			})
-			.error(function(err) {
-				console.log('err w/challenge context', err);
-			})
-	}
+    ChallengeService.getChallengeContext(challengeId)
+      .success(function(res) {
+        $scope.challengeName = res.challenge.name;
+        console.log('challengeName', challengeName);
+        console.log('challenge context', res);
+        $scope.allChallengers = res.challenge.Challengers;
+        console.log('all challengers', $scope.allChallengers)
+      })
+      .error(function(err) {
+        console.log('err w/challenge context', err);
+      })
+  }
 
-  $scope.challengeActive = function(){
-    if (Date.now() < expireTime){
+  $scope.challengeActive = function() {
+    if (Date.now() < expireTime) {
       return true;
-    }else{
+    } else {
       return false;
     }
   };
 
   ionic.Platform.ready(function() {
 
-    $scope.acceptChallenge = function(){
+    $scope.acceptChallenge = function() {
 
       // alert('')
       // UserStatsService.updateAcceptStat($localStorage.activeUserId)
       //   .success(function (res){
-          // console.log('users accepted stat is updated', res);
+      // console.log('users accepted stat is updated', res);
 
-           Camera.getPicture({
-              quality: 75,
-              targetWidth: 1024,
-              targetHeight: 1024,
-              destinationType: 0,
-              encodingType: 0,
-              saveToPhotoAlbum: false,
-              correctOrientation: true
-            })
-            .then(function(imageData) {
+      Camera.getPicture({
+          quality: 75,
+          targetWidth: 1024,
+          targetHeight: 1024,
+          destinationType: 0,
+          encodingType: 0,
+          saveToPhotoAlbum: false,
+          correctOrientation: true
+        })
+        .then(function(imageData) {
 
-              alert('imageData', imageData);
+          alert('imageData', imageData);
 
-              if (imageData) {
-                PictureService.sendImageToServer(imageData, challengerId)
-                  .success(function(res) {
-                    DataSharingService.errorLog.sendImageToServer = 'no error';
-                    $state.go('app.challenge-in-progress',{
-                      activeChallengeId: challengeId,
-                      activeChallengeExpireTime: expireTime
-                    });
-                  })
-                  .error(function(error) {
-                    DataSharingService.errorLog.sendImageToServer = 'error';
-                  })
+          if (imageData) {
+            PictureService.sendImageToServer(imageData, challengerId)
+              .success(function(res) {
+                DataSharingService.errorLog.sendImageToServer = 'no error';
+                $state.go('app.challenge-in-progress', {
+                  activeChallengeId: challengeId,
+                  activeChallengeExpireTime: expireTime
+                });
+              })
+              .error(function(error) {
+                DataSharingService.errorLog.sendImageToServer = 'error';
+              })
 
-              } else {
-                DataSharingService.errorLog.sendImageToServer = 'no image data';
-              }
-            });
-        // })
-        // .error(function (err){
-        //   console.log('error with updating the user accept stat', err);
-        // })
+          } else {
+            DataSharingService.errorLog.sendImageToServer = 'no image data';
+          }
+        });
+      // })
+      // .error(function (err){
+      //   console.log('error with updating the user accept stat', err);
+      // })
 
     };
 
 
-  };
+  });
 
 });
