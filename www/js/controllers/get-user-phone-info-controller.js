997dec2c2024ecdd1ce59d47814823eb840cf885
Judah Lamm <Judah.Lamm@gmail.com>
2015-08-16 15:18:03 -1000

    fixed css

--- www/js/controllers/get-user-phone-info-controller.js
+++ www/js/controllers/get-user-phone-info-controller.js
@@ -7,14 +7,10 @@ angular.module('starter')
   // var user_id =  $localStorage.activeUserId;
 
   $scope.updateUserPhoneInfo = function (user_info){
-<<<<<<< HEAD:www/js/controllers/get-user-phone-info.js
 
     //validate phone number
     if(validationService.phoneNumberVal(user_info.phone)){
-
       user_info.phone = validationService.parseNumber(user_info.phone);
-      console.log('parsed phone number being sent', user_info.phone)
-
       UserService.updateUserPhoneInfo($localStorage.activeUserId, user_info)
         .success(function (res){
           console.log('updated user info', res);
@@ -24,40 +20,23 @@ angular.module('starter')
           alert(error);
           console.log('error with updating a user phone info', error);
         })
-
     }
     //if phone number invalid display error message
     else{
       $scope.showErrorMessage = true;
       user_info.phone = '';
-
-
     }
-
-
-=======
-    console.log('$localStorage.activeUserId',$localStorage.activeUserId);
-    UserService.updateUserPhoneInfo($localStorage.activeUserId, user_info)
-      .success(function (res){
-        console.log('updated user info', res);
-        $state.go('app.landing');
-      })
-      .error(function (error){
-        alert(error);
-        console.log('error with updating a user phone info', error);
-      })
->>>>>>> develop:www/js/controllers/get-user-phone-info-controller.js
   }
 
 
   $scope.providers = [];
   ProviderService.getAllProviders()
-  .success(function (res){
-    console.log('providers', res)
-    $scope.providers = res;
-  })
-  .error(function (err){
-    console.log('err', err);
-  })
+    .success(function (res){
+      console.log('providers', res)
+      $scope.providers = res;
+    })
+    .error(function (err){
+      console.log('err', err);
+    })
 
 })
