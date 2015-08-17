3def214503ac0e88bf91c191a5e3aca749fd6740
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-08-17 11:22:49 +0000

    updated camera to set correct orientation to true

--- www/js/controllers/user-challenged-controller.js
+++ www/js/controllers/user-challenged-controller.js
@@ -1,51 +1,77 @@
 angular.module('starter')
 
-.controller('user-challenged-controller', function($location, $localStorage, $scope, $ionicPlatform, ChallengeService, $stateParams, $rootScope, Camera, $timeout, UserStatsService) {
-  $scope.allChallengers = [];
-  var challengeId = $stateParams.activeChallengeId;
+.controller('user-challenged-controller', function($location, $localStorage, $scope, $ionicPlatform, ChallengeService, $stateParams,$rootScope, Camera, $timeout, UserStatsService) {
+	$scope.allChallengers = [];
+	var challengeId = $stateParams.activeChallengeId;
   var expireTime = $stateParams.activeChallengeExpireTime;
   $scope.expireTime = expireTime;
-  var challengerId = $localStorage.activeUserId;
+  var challengerId; //needs to be defined somehow
 
   $scope.getChallengeContext = function() {
-    ChallengeService.getChallengeContext(challengeId)
-      .success(function(res) {
-        $scope.challengeName = res.challenge.name;
-        console.log('challengeName', challengeName);
-        console.log('challenge context', res);
-        $scope.allChallengers = res.challenge.Challengers;
-        console.log('all challengers', $scope.allChallengers)
-      })
-      .error(function(err) {
-        console.log('err w/challenge context', err);
-      })
-  }
+		ChallengeService.getChallengeContext(challengeId)
+			.success(function(res) {
+				$scope.challengeName = res.challenge.name;
+				console.log('challengeName', challengeName);
+				console.log('challenge context', res);
+				$scope.allChallengers = res.challenge.Challengers;
+				console.log('all challengers', $scope.allChallengers)
+			})
+			.error(function(err) {
+				console.log('err w/challenge context', err);
+			})
+	}
 
-  $scope.challengeActive = function() {
-    if (Date.now() < expireTime) {
+  $scope.challengeActive = function(){
+    if (Date.now() < expireTime){
       return true;
-    } else {
+    }else{
       return false;
     }
   };
 
 
-  $scope.acceptChallenge = function() {
+  $scope.acceptChallenge = function(){
 
-    // UserStatsService.updateAcceptStat($localStorage.activeUserId)
-    //   .success(function(res) {
-    //     alert('update stat')
-    //     console.log('users accepted stat is updated', res);
-    //     $scope.getPhoto();
-    //   })
-    //   .error(function(err) {
-    //     console.log('error with updating the user accept stat', err);
-    //   })
-    $scope.getPhoto();
+    UserStatsService.updateAcceptStat($localStorage.activeUserId)
+      .success(function (res){
+        console.log('users accepted stat is updated', res);
+
+         Camera.getPicture({
+            quality: 75,
+            targetWidth: 1024,
+            targetHeight: 1024,
+            destinationType: 0,
+            encodingType: 0,
+            saveToPhotoAlbum: false,
+            correctOrientation: true
+          })
+          .then(function(imageData) {
+
+            if (imageData) {
+              PictureService.sendImageToServer(imageData, challengerId)
+                .success(function(res) {
+                  DataSharingService.errorLog.sendImageToServer = 'no error';
+                  $state.go('app.challenge-in-progress',{
+                    activeChallengeId: challengeId,
+                    activeChallengeExpireTime: expireTime
+                  });
+                })
+                .error(function(error) {
+                  DataSharingService.errorLog.sendImageToServer = 'error';
+                })
+
+            } else {
+              DataSharingService.errorLog.sendImageToServer = 'no image data';
+            }
+          });
+      })
+      .error(function (err){
+        console.log('error with updating the user accept stat', err);
+      })
 
   };
 
-  $scope.getPhoto = function() {
+$scope.getPhoto = function() {
     Camera.getPicture({
         quality: 75,
         targetWidth: 1024,
@@ -76,7 +102,7 @@ angular.module('starter')
   };
 
 
-  $ionicPlatform.ready(function() {
-    $scope.getChallengeContext();
-  });
+	$ionicPlatform.ready(function() {
+		$scope.getChallengeContext();
+	});
 });
