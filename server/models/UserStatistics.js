cb68ac8dba52ca16d8f8d02b5f8ad1b3fd564931
danturne <daniel.turner@gmail.com>
2015-08-16 17:06:21 -1000

    added user_statistics table and routes, server-side messaging revised for new public user information exposure

---
+++ server/models/UserStatistics.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes) {
+
+  var UserStatistics = sequelize.define("UserStatistics", {
+
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    challenges_started: DataTypes.INTEGER,
+    challenges_accepted: DataTypes.INTEGER,
+    challenges_declined: DataTypes.INTEGER
+
+  },{
+    underscored: true,
+    tableName: "user_statistics",
+  });
+
+  return UserStatistics;
+};
