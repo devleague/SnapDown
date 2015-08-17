c6608a081b47efac131f0950be4a2335199bffc7
Constantino <Coproductions@Constantins-MacBook-Pro.local>
2015-08-16 15:02:18 -1000

    phone validation working

--- www/js/validationService.js
+++ www/js/validationService.js
@@ -5,11 +5,14 @@ angular.module('starter')
   .service('validationService', function(){
 
     this.phoneNumberVal = function(value){
+      if(!value.match(/\d/g)){
+        return false;
+      }
       return value.match(/\d/g).length===10;
     }
 
     this.parseNumber = function(value){
-      return = value.match(/\d/g).join();
+      return  value.match(/\d/g).join('');
     }
 
 
