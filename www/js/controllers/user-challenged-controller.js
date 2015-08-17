38a58c0c3c40539524475db0f5de1b6cda4afcee
Judah Lamm <Judah.Lamm@gmail.com>
2015-08-17 11:56:25 -1000

    fixed the syntax

--- www/js/controllers/user-challenged-controller.js
+++ www/js/controllers/user-challenged-controller.js
@@ -7,6 +7,8 @@ angular.module('starter')
   $scope.expireTime = expireTime;
   var challengerId = $stateParams.challengerId;
 
+  $scope.getChallengeContext();
+
   $scope.getChallengeContext = function() {
 		ChallengeService.getChallengeContext(challengeId)
 			.success(function(res) {
@@ -31,84 +33,51 @@ angular.module('starter')
 
   ionic.Platform.ready(function() {
 
-  $scope.acceptChallenge = function(){
+    $scope.acceptChallenge = function(){
 
-    // alert('')
-    // UserStatsService.updateAcceptStat($localStorage.activeUserId)
-    //   .success(function (res){
-        // console.log('users accepted stat is updated', res);
+      // alert('')
+      // UserStatsService.updateAcceptStat($localStorage.activeUserId)
+      //   .success(function (res){
+          // console.log('users accepted stat is updated', res);
 
-         Camera.getPicture({
-            quality: 75,
-            targetWidth: 1024,
-            targetHeight: 1024,
-            destinationType: 0,
-            encodingType: 0,
-            saveToPhotoAlbum: false,
-            correctOrientation: true
-          })
-          .then(function(imageData) {
+           Camera.getPicture({
+              quality: 75,
+              targetWidth: 1024,
+              targetHeight: 1024,
+              destinationType: 0,
+              encodingType: 0,
+              saveToPhotoAlbum: false,
+              correctOrientation: true
+            })
+            .then(function(imageData) {
 
-            alert('imageData', imageData);
+              alert('imageData', imageData);
 
-            if (imageData) {
-              PictureService.sendImageToServer(imageData, challengerId)
-                .success(function(res) {
-                  DataSharingService.errorLog.sendImageToServer = 'no error';
-                  $state.go('app.challenge-in-progress',{
-                    activeChallengeId: challengeId,
-                    activeChallengeExpireTime: expireTime
-                  });
-                })
-                .error(function(error) {
-                  DataSharingService.errorLog.sendImageToServer = 'error';
-                })
+              if (imageData) {
+                PictureService.sendImageToServer(imageData, challengerId)
+                  .success(function(res) {
+                    DataSharingService.errorLog.sendImageToServer = 'no error';
+                    $state.go('app.challenge-in-progress',{
+                      activeChallengeId: challengeId,
+                      activeChallengeExpireTime: expireTime
+                    });
+                  })
+                  .error(function(error) {
+                    DataSharingService.errorLog.sendImageToServer = 'error';
+                  })
 
-            } else {
-              DataSharingService.errorLog.sendImageToServer = 'no image data';
-            }
-          });
-      // })
-      // .error(function (err){
-      //   console.log('error with updating the user accept stat', err);
-      // })
+              } else {
+                DataSharingService.errorLog.sendImageToServer = 'no image data';
+              }
+            });
+        // })
+        // .error(function (err){
+        //   console.log('error with updating the user accept stat', err);
+        // })
+
+    };
+
 
   };
-
-
-  $scope.getChallengeContext();
-
-}
-
-// $scope.getPhoto = function() {
-//     Camera.getPicture({
-//         quality: 75,
-//         targetWidth: 1024,
-//         targetHeight: 1024,
-//         destinationType: 0,
-//         encodingType: 0,
-//         saveToPhotoAlbum: false,
-//         correctOrientation: true
-//       })
-//       .then(function(imageData) {
-
-//         if (imageData) {
-//           PictureService.sendImageToServer(imageData, challengerId)
-//             .success(function(res) {
-//               DataSharingService.errorLog.sendImageToServer = 'no error';
-//               $state.go('app.select-challenger');
-//             })
-//             .error(function(error) {
-//               DataSharingService.errorLog.sendImageToServer = 'error';
-//               $state.go('app.select-challenger');
-//             })
-
-//         } else {
-//           DataSharingService.errorLog.sendImageToServer = 'no image data';
-//           $state.go('app.select-challenger');
-//         }
-//       });
-//   };
-
 
 });
